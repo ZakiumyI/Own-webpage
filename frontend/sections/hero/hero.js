3d0ad1c97f0fe23fe30a7124e8e28b9135a6014a
Zakiumy <carlos.bustos2401@alumnos.ubiobio.cl>
2026-02-06 01:37:59 -0300

    bg features

--- frontend/sections/hero/hero.js
+++ frontend/sections/hero/hero.js
@@ -2,5 +2,10 @@ export async function HeroSection() {
   const html = await fetch("/sections/hero/hero.html").then(r => r.text());
   const container = document.createElement("div");
   container.innerHTML = html;
+
+  requestAnimationFrame(() => initHeroScroll(container));
+
   return container;
 }
+
+
